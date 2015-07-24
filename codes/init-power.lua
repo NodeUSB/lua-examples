9884cf98a2c25016eb6aeedce42ff09b8c090828
mikewen <mikewen@users.noreply.github.com>
2015-07-24 00:54:31 -0400

    Update init-power.lua

--- codes/init-power.lua
+++ codes/init-power.lua
@@ -1,5 +1,5 @@
 
-local pKey="xxxxxxxxxxxxxxxxxxx"
+local pKey="xxxxxxxxxxxxxxxxxxx"  --replace with Access token
 local host="asnodes.appspot.com"
 
 local pr = require("postrun")
@@ -24,7 +24,7 @@ tmr.alarm(1,8000,1,function()
      local mac = wifi.sta.getmac()
 
       --server can return lua code to turn off LED, etc
-     local sPostData = string.format([[a=led&ip=%s&m=%s&mac=%s&k=%s]],ip,m,mac,pKey)
+     local sPostData = string.format([[a=led&ip=%s&m=%s&mac=%s&at=%s]],ip,m,mac,pKey)
 
      print(sPostData)
      pr.post(host,sPostData)
